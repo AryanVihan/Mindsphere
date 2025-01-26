229e9b839edb73a24e846683d1c8930010a48eff
AryanVihan <aryanvihan888@gmail.com>
2025-01-26 09:54:15 +0530

    chatbot added

---
+++ chatbot.py
@@ -0,0 +1,39 @@
+import os
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyA3YdsRKMUb_uDq9sEwuuWkRIeYRHKtoYY")
+
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 40,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-2.0-flash-exp",
+  generation_config=generation_config,
+)
+
+history = []
+
+print("Bot: Hello, how can I help you?")
+
+while True:
+
+    user_input = input("You: ")
+
+    chat_session = model.start_chat(
+        history = history
+    )
+
+    response = chat_session.send_message(user_input)
+
+    model_response = response.text
+
+    print(f'Bot: {model_response}')
+    print()
+
+    history.append({"role": "user", "parts": [user_input]})
+    history.append({"role": "model", "parts": [model_response]})
